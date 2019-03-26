0eff543f619addb25a37dfceb35f0847b118b5fb
Brian K <ketchem2@wisc.edu>
2019-03-26 14:31:01 -0400

    Renamed js file

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// Anonymous function to keep all variables local
+(function(){
+
+    //variables for data join
+
+
+    //chart frame dimensions
+
+
+    //create a scale to size bars proportionally to frame
+
+
+    //begin script when window loads
+
+
+    // Setup the map
+        // Add the data sets
+        // Manipulate data sets
+            // Create map background
+            // Join data together
+            // Create the color scale
+            // Draw the countries
+            // Add chart
+            // Create interface elements
+});
