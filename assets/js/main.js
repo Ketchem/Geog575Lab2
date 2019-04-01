211efd457cbec487f89a8ac1e61efe0f6730ef46
Brian K <ketchem2@wisc.edu>
2019-04-01 08:49:43 -0400

    Added new data and created map container with country outlines

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@
 (function(){
 
     //variables for data join
-
+    var attrArray = ["TotalNumberMedals","TotalGold","GoldSummer","GoldWinter","TotalSilver","SilverSummer",
+        "SilverWinter", "TotalBronze","BronzeSummer", "BronzeWinter"];
+    var expressed = attrArray[0];
 
     //chart frame dimensions
 
@@ -11,9 +13,47 @@
 
 
     //begin script when window loads
-
+    window.onload = setMap();
 
     // Setup the map
+    function setMap(){
+
+        // Map frame dimensions
+        var width = window.innerWidth * 0.75,
+            height = 690;
+
+        // Create new svg container for the map
+        var map = d3.select("body")
+            .append("svg")
+            .attr("class", "map")
+            .attr("width", width)
+            .attr("height", height);
+
+        // Create the projection
+        var projection = d3.geoCylindricalEqualArea();
+
+        var path = d3.geoPath()
+            .projection(projection);
+
+        // Load the data sets
+        var dataSets = [];
+        dataSets.push(d3.csv("assets/data/medalsByCountry.csv"));
+        dataSets.push(d3.json("assets/data/worldCountries.topojson"));
+
+        // Work with the data sets
+        console.log(dataSets);
+        Promise.all(dataSets).then(function(values){
+            console.log(values);
+           var csvData = values[0];
+           var worldCountries = topojson.feature(values[1], values[1].objects.worldCountries);
+
+            var countries = map.append("path")
+                .datum(worldCountries)
+                .attr("class", "countries")
+                .attr("d", path);
+        });
+
+    };
         // Add the data sets
         // Manipulate data sets
             // Create map background
@@ -22,4 +62,4 @@
             // Draw the countries
             // Add chart
             // Create interface elements
-});
+}) ();
